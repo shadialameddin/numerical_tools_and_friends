abc948b399a41d88ed9393cac91e9c88519d761c
Shadi Alameddin <shadialameddin@gmail.com>
2020-07-30 22:05:47 +0200

    monitor gpu

--- python/tensorflow_gpu/tensorflow_gpu_test.sh
+++ python/tensorflow_gpu/tensorflow_gpu_test.sh
@@ -5,3 +5,11 @@ CUDA_VISIBLE_DEVICES=0 python tensorflow_gpu_test.py & CUDA_VISIBLE_DEVICES=0 py
 
 # profile the code to see if it's reasonable to send the data to the GPU
 nvprof python tensorflow_gpu_test.py
+
+
+# watch -n0.1 nvidia-smi
+# nvidia-smi -l 1
+# pip install gpustat
+# gpustat -cp -i 1
+
+# TF_CPP_MIN_LOG_LEVEL=2
